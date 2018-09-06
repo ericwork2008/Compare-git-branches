cc1a3631c469240919305a9723cf8bf65bfa406b
ericwork2008 <ericwork2008@gmail.com>
2018-09-05 17:58:27 -0700

    Update compare-branches.py

--- compare-branches.py
+++ compare-branches.py
@@ -3,6 +3,7 @@
 Copyright (c) 2012 Rowan Wookey <admin@rwky.net>
           (c) 2013 Bernd Schubert <bernd.schubert@itwm.fraunhofer.de>
 
+           
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -14,16 +15,24 @@ import sys, subprocess, getopt
 
 gitLogCmd = ['git','log','--pretty=oneline','--no-merges']
 gitAuthorCmd = ['git', 'show', '-s', '--format=(%an) %aD']
+gitCommitMsgCmd = ['git', 'log', '-1', '--pretty=%B']
+gitFilterCmd  = ['git', 'show', '--pretty=medium', '--name-only']
 
 branchAOnly  = False
 branchBOnly  = False
 reversedOrder = False
+
+filterString  = ""
+path = ""
+
+changeIdLine = 'Change-Id:'
 
 # just a basic commit object
 class gitCommit:
     def __init__(self, commitID, commitSubject):
         self.commitID      = commitID
         self.commitSubject = commitSubject
+        self.changeID  = ""
 
     def getCommitID(self):
         return self.commitID
@@ -31,27 +40,59 @@ class gitCommit:
     def getCommitSubject(self):
         return self.commitSubject
 
+    def addChangeID(self, ID):
+        self.changeID = ID
+
+    def getChangeID(self):
+        return self.changeID
+
 
 class Branch:
     def __init__(self, branchName):
         self.branchName = branchName
         self.patchIdDict = {} # for fast search
         self.commitList = []  # list of gitCommit objects
+        self.commitObjDict  = {}  # list of gitCommit objects
         self.missingList = [] # list of missing commitIDs of this branch
+
+    def parserMsgData(self, commitID, commitObj):
+        commitMsg = subprocess.check_output(gitCommitMsgCmd + [commitID])
+        #print "commitMsg: " + commitMsg
+        searchRegEx  = re.compile(cherryPickLine)
+        searchRegEx1  = re.compile(changeIdLine)
+
+        for line in commitMsg.splitlines():
+            if searchRegEx.search(line):
+                cherryPickID = searchRegEx.split(line)[1]
+                #print "cherryPickID: " + cherryPickID
+                # remove closing bracket
+                cherryPickID = re.sub('\)$', '', cherryPickID)
+                commitObj.addCherryPickID(cherryPickID)
+
+            if searchRegEx1.search(line):
+                changeID = searchRegEx.split(line)[0]
+                # remove Change-Id:
+                changeID = re.sub('Change-Id: ', '', changeID)
+                commitObj.addChangeID(changeID)
+                #print "changeID: " + changeID
 
     def addCommit(self, commitID, commitSubject):
         commitObj = gitCommit(commitID, commitSubject)
-        self.commitList.append(commitObj)
 
         # we don't use "git show", as it includes the message 
         diff = subprocess.check_output(['git', 'show', commitID])
         proc = subprocess.Popen(['git', 'patch-id'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         patchID = proc.communicate(input=diff)[0].split(' ')[0]
 
+        self.parserMsgData(commitID,commitObj)
+
         # print self.branchName + ': Adding: ' + patchID + ' : ' + commitID
-
+        self.commitList.append(commitID)
+        self.commitObjDict[commitID] = commitObj
+        self.commitObjDict[commitObj.getChangeID()] = commitObj
         self.patchIdDict[patchID] = commitID
-
+        #print self.branchName + ': Adding: patchID ' + patchID + ' : commitID ' + commitID + ': Change-ID ' + commitObj.getChangeID()
+ 
     def addLogLine(self, logLine):
         commitID      = logLine[:40]
         commitSubject = logLine[41:]
@@ -124,18 +165,23 @@ def usage():
           -b <branch-name> 
                 The name of branch b.
           -A 
-                List commits missing from branch a only
+                List all commits in branch b but doesn't present in branch a.
           -B 
-                List commits missing from branch b only
+                 List all commits in branch a but doesn't present in branch b.
           -r
                 Print in reverse order (older (top) to newer (bottom) )
           -t
                 How far back in time to go (passed to git log as --since) i.e. '1 month ago'
+          -p
+                Specific path or file
+          -F
+                Specifi a filter, the filter will check changed file name and commit comments
+
         '''
 
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "ha:b:BArt:")
+    opts, args = getopt.getopt(sys.argv[1:], "ha:b:BArt:F:p:")
 except:
     usage()
     sys.exit()
@@ -157,6 +203,12 @@ for opt,arg in opts:
         reversedOrder = True
     if opt == '-t':
         since = arg
+    if opt == '-F':
+        filterString = arg
+        print "include file:"+filterString
+    if opt == '-p':
+        path = arg
+        print "PATH:"+path
 
 
 if 'branchAName' not in globals() or 'branchBName' not in globals():
